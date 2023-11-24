3269a62c8df591ceced0fbb08f34b2428069d595
Gagannn126 <Gagannn126@gmail.com>
2023-11-24 10:41:10 +0530

    update

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+streamlit.header("Zena's amazing athleisure catalog")
+
+streamlit.text_input('Pick a sweatsuit color or style',red)
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
